d02a669ff036cd4e19376ff7197931b12d544ac7
taketee81 <161117537+taketee81@users.noreply.github.com>
2025-01-01 19:18:31 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
   //project_name : XPLOADER
-// @author : TYLOR
+// @author : TAKE TEE 
 // @youtube : https://www.youtube.com/@heyits_tylor
 // @instagram : heyits_tylor
 // @telegram : t.me/heyits_tylor
 // @github : Dark-Xploit
 // @tiktok : heyits_tylor
-// @whatsapp : +254754783972
+// @whatsapp : +23274448130 
 //*
 //* 
 //=================================================//
@@ -15,7 +15,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
 //=================================================//
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'XPLOADER~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoic0RYY2FrMUhSRE9JQXVPWUJnY3hxdGhnWityR0NMZmtLWDhrK01GYWEwbz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMUpObS8wcXR3REExUEh0Qk5ob0V5ZzBBK094SXZhbGFMMk5TL1lKREtHVT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJBRUVZZ2VYOE9UbFcwaXBRckc0aTVPRGxUS1h6MWJjT2VnVnpQUFFFUkc4PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJUUFV6N3pqR1FSZXRMR2owM2psWU1PY01NMTg0YVZlaXUxbElqd1d5ZjJZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InFNWUZhZzROZ0ZBVXRGZk1mQjdJZ09DOVZqdi9mWGtQR3BaSjEySjZKRVU9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImZvQmxhbVMyV3hxMlJWMEdXTStqSjhzUUp5UjZ6aWZ2TG9PV2MyeERUakU9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia1B2M1o4bjVGWTh6ZS9mbWJteGcyM0UwSzc2eUhsZ1FiaDdjZEIxRlFHOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiN1BtaDNROVVXd0Y0Nk8wZzBoTjh3WWNxM3BHMVNmWEFVVzAxdk1CdytFMD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImY4bzdQOXhzY1JIdGhydUpLc3QyQmV2UGlQaWIveUM5eGwxSlk1enBRNmlHVU5FK3lNRThrM2dhZW5ULzJkOGRNWCtRZlAycDhBRFlwTVZqRkVCWGhBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTI1LCJhZHZTZWNyZXRLZXkiOiJQQ3RGU05VRVNEK21nanBaV0NMcG1ueXFhd1RnTTBlUVZUUk55TWMwRGtnPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJQNnRFMmd3TlNzZXQydDNRWklxcS1nIiwicGhvbmVJZCI6IjFhNDNlZmMzLWE5ODctNDk4MS1hNmUxLWI3MWZmODEwYjZlNyIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI4MEJpWTVZVFN2TFNvZkQyM1JyZ2R3VGRickU9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMlNmWHIweWo5WWJETHFJU2NWa0dSV3N1Yk9vPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6Ik05RThRQ0hIIiwibWUiOnsiaWQiOiIyMzI3NDQ0ODEzMDo0MkBzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiJNciBUYWtlIFRlZSIsImxpZCI6IjE0MTM1MjAwNDIyNzEwMzo0MkBsaWQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ01xRG9kTURFS2FuMXJzR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6Ik9Yai9LRHNIT2JQODJqZ0RJZmJEbEhRcUYwVnFQSmRzUVI4a091ekdJUUE9IiwiYWNjb3VudFNpZ25hdHVyZSI6IkQvbThDY2JBbG82dEpBSXBSV2gzaGJueXZCaWM5K1A4b2xSNXlhYm5XYUh3TFNjY0ZCc0FLRDh5VkE1dG9pMHNEbnlUY21xL1lGaGhqMnFYQnZGT0FnPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJwTm5DTkh4VFUyTFRUTVV1emtCalVXMnlUMEhVeVp3TkFyQjAxNlVQL0xXLzg4RlU0Qk5hQXhhcTdHeDZiVUVZczN0Ry9tL1BzUG5ESEF4WitBbXhpdz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjIzMjc0NDQ4MTMwOjQyQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlRsNC95ZzdCem16L05vNEF5SDJ3NVIwS2hkRmFqeVhiRUVmSkRyc3hpRUEifX1dLCJwbGF0Zm9ybSI6InNtYmEiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MzU3NTg3NzEsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBUHJTIn0=' 
 //Enter your Xploader session id here; must start with XPLOADER~
 
 //=================================================//
@@ -23,11 +23,11 @@ global.botname = process.env.BOT_NAME || '᙭ᑭᒪOᗩᗪᗴᖇ ᗷOT'
 //Your desired bot name
 
 //=================================================//
-global.ownernumber = process.env.OWNER_NUMBER || '254754783972' 
+global.ownernumber = process.env.OWNER_NUMBER || '23274448130'
 //Type your number here
 
 //=================================================//
-global.ownername = process.env.OWNER_NAME || 'Tylor' 
+global.ownername = process.env.OWNER_NAME || 'TAKE TEE'
 //Type your name here
 
 //=================================================//
@@ -79,7 +79,7 @@ global.timezones = process.env.TIMEZONE || "Africa/Nairobi"
 //Don't edit this if you don't know!
 
 //=================================================//
-global.countrycode = process.env.COUNTRY_CODE || '254' 
+global.countrycode = process.env.COUNTRY_CODE || '232' 
 //set your country code for functionality of blockforeign and antiforeign commands
 
 //=================================================//
